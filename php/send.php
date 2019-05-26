b1d9d01616fb5f804753ecaaf3309bc25d01dab4
0ilia <ilia1mail2martinkevi4@gmail.com>
2019-05-26 14:38:50 +0300

    addHashmd5

--- php/send.php
+++ php/send.php
@@ -1,20 +1,27 @@
 <?php
 require "bd.php";
+
+$saltForMD5 = "KLdj1KQ33~QPT7@Rj#";
+function Getmd5($code,$saltForMD5 ="KLdj1KQ33~QPT7@Rj#"){
+
+    $strUtf32 = mb_convert_encoding( $saltForMD5.$code, "UTF-32LE");
+    return md5($strUtf32);
+}
 $name = trim(htmlentities($_POST['nikN'], ENT_QUOTES));
 $text_mess = trim(htmlentities($_POST['textN'], ENT_QUOTES));
 $code = trim(htmlentities($_POST['passN'], ENT_QUOTES));
 if($name==NULL){
-    $name = 'Anonymous';
+    $name = 'Аноним';
 }
 if ((strlen($code)>4)&&(strlen($code)<20)&&(strlen($text_mess)>1)&&(strlen($text_mess)<255)) {
    $bd_mess = R::dispense('message');
     $bd_mess->name = $name;
-    $bd_mess->code = $code;
+    $bd_mess->code = Getmd5($code);
     $bd_mess->text_message = $text_mess;
     R::store($bd_mess);
   }
 
-$search = R::find('message', 'code = ?', array($code));
+$search = R::find('message', 'code = ?', array(Getmd5($code)));
 if ((strlen($code)>4)&&(strlen($code)<20)) {
    foreach ($search as $key => $value) {
         echo $value['name'] . ': ' . $value['text_message'] . "<br>";
